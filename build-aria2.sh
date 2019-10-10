e3a9d8079e59165018c3dc85aaaffd71a639d234
qweaszxcdf <qweaszxcdf@users.noreply.github.com>
2019-10-10 11:38:17 +0800

    Update build-aria2.sh

--- build-aria2.sh
+++ build-aria2.sh
@@ -10,12 +10,21 @@ MINGW64)
     ;;
 esac
 
-# workaround for appveyor
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
+# workaround git user name and email not set
+GIT_USER_NAME="$(git config --global user.name)"
+GIT_USER_EMAIL="$(git config --global user.email)"
+if [[ "${GIT_USER_NAME}" = "" ]]; then
+    git config --global user.name "Name"
+fi
+if [[ "${GIT_USER_EMAIL}" = "" ]]; then
+    git config --global user.email "you@example.com"
+fi
 
-pacman -S --noconfirm --needed $MINGW_PACKAGE_PREFIX-toolchain \
-    $MINGW_PACKAGE_PREFIX-expat $MINGW_PACKAGE_PREFIX-gmp $MINGW_PACKAGE_PREFIX-c-ares
+pacman -S --noconfirm --needed $MINGW_PACKAGE_PREFIX-gcc \
+    $MINGW_PACKAGE_PREFIX-expat \
+    $MINGW_PACKAGE_PREFIX-gmp \
+    $MINGW_PACKAGE_PREFIX-c-ares \
+    $MINGW_PACKAGE_PREFIX-winpthreads
 
 PREFIX=/usr/local/$HOST
 CPUCOUNT=$(grep -c ^processor /proc/cpuinfo)
@@ -44,9 +53,10 @@ get_last_version() {
     echo "$ret"
 }
 
+# expat
 expat_ver="$(clean_html_index https://sourceforge.net/projects/expat/files/expat/ 'expat/[0-9]+\.[0-9]+\.[0-9]+')"
 expat_ver="$(get_last_version "${expat_ver}" expat '2\.\d+\.\d+')"
-expat_ver="${expat_ver:-2.2.5}"
+expat_ver="${expat_ver:-2.2.8}"
 wget -c --no-check-certificate "https://downloads.sourceforge.net/project/expat/expat/${expat_ver}/expat-${expat_ver}.tar.bz2"
 tar xf "expat-${expat_ver}.tar.bz2"
 cd "expat-${expat_ver}"
@@ -59,8 +69,9 @@ make install -j$CPUCOUNT
 cd ..
 rm -rf "expat-${expat_ver}"
 
+# sqlite
 sqlite_ver=$(clean_html_index_sqlite "https://www.sqlite.org/download.html")
-[[ ! "$sqlite_ver" ]] && sqlite_ver="2018/sqlite-autoconf-3240000.tar.gz"
+[[ ! "$sqlite_ver" ]] && sqlite_ver="2019/sqlite-autoconf-3290000.tar.gz"
 sqlite_file=$(echo ${sqlite_ver} | grep -ioP "(sqlite-autoconf-\d+\.tar\.gz)")
 wget -c --no-check-certificate "https://www.sqlite.org/${sqlite_ver}"
 tar xf "${sqlite_file}"
@@ -76,10 +87,11 @@ make install -j$CPUCOUNT
 cd ..
 rm -rf "${sqlite_name}"
 
+# c-ares: Async DNS support
 [[ ! "$cares_ver" ]] &&
     cares_ver="$(clean_html_index https://c-ares.haxx.se/)" &&
     cares_ver="$(get_last_version "$cares_ver" c-ares "1\.\d+\.\d")"
-cares_ver="${cares_ver:-1.14.0}"
+cares_ver="${cares_ver:-1.15.0}"
 echo "c-ares-${cares_ver}"
 wget -c --no-check-certificate "https://c-ares.haxx.se/download/c-ares-${cares_ver}.tar.gz"
 tar xf "c-ares-${cares_ver}.tar.gz"
@@ -95,10 +107,11 @@ make install -j$CPUCOUNT
 cd ..
 rm -rf "c-ares-${cares_ver}"
 
+# libssh2
 [[ ! "$ssh_ver" ]] &&
     ssh_ver="$(clean_html_index https://libssh2.org/download/)" &&
     ssh_ver="$(get_last_version "$ssh_ver" tar.gz "1\.\d+\.\d")"
-ssh_ver="${ssh_ver:-1.8.0}"
+ssh_ver="${ssh_ver:-1.9.0}"
 echo "${ssh_ver}"
 wget -c --no-check-certificate "https://libssh2.org/download/libssh2-${ssh_ver}.tar.gz"
 tar xf "libssh2-${ssh_ver}.tar.gz"
@@ -108,18 +121,25 @@ cd "libssh2-${ssh_ver}"
     --enable-static \
     --prefix=/usr/local/$HOST \
     --host=$HOST \
-    --without-openssl \
-    --with-wincng \
+    --with-crypto=wincng \
     LIBS="-lws2_32"
 make install -j$CPUCOUNT
 cd ..
 rm -rf "libssh2-${ssh_ver}"
 
-git clone https://github.com/aria2/aria2 --depth=1 --config http.sslVerify=false
-wget https://raw.githubusercontent.com/myfreeer/aria2-build-msys2/master/aria2-0005-option-add-option-to-retry-on-http-4xx.patch
-cd aria2
-git am ../*.patch
-autoreconf -i
+if [[ -d aria2 ]]; then
+    cd aria2
+    git checkout master
+    git reset --hard origin || git reset --hard
+    git pull
+else
+    git clone https://github.com/aria2/aria2 --depth=1 --config http.sslVerify=false
+    cd aria2
+fi
+git checkout -b patch
+git am  ../*.patch
+
+autoreconf -fi || autoreconf -fiv
 ./configure \
     --host=$HOST \
     --prefix=$PREFIX \
@@ -139,7 +159,10 @@ autoreconf -i
     --with-cppunit-prefix=$PREFIX \
     ARIA2_STATIC=yes \
     CPPFLAGS="-I$PREFIX/include" \
-    LDFLAGS="-L$PREFIX/lib" \
+    LDFLAGS="-L$PREFIX/lib -Wl,--gc-sections,--build-id=none" \
     PKG_CONFIG_PATH="$PREFIX/lib/pkgconfig"
 make -j$CPUCOUNT
-strip src/aria2c.exe
+strip -s src/aria2c.exe
+git checkout master
+git branch patch -D
+cd ..
